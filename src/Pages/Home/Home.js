1eea9ad5fd487024588ff87ec8001b4d9c2b0493
Amisha Walia <amisha1530.cse18@chitkara.edu.in>
2022-04-28 00:41:04 +0530

    basic chatapp template

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import Messenger from "../../Components/Messenger/Messenger";
+import Topbar from "../../Components/Topbar/Topbar";
+
+const Home = () => {
+    return <>
+        <div>
+            <Topbar/>
+            <Messenger/>
+        </div>
+    </>
+}
+export default Home;
